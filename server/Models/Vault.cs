0e7e05872d28252a8c108d4d5c9b2c555d368acd
Paul Fontes <pgfon22@gmail.com>
2025-10-24 16:16:47 -0600

    vaults pages made

---
+++ server/Models/Vault.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace keepr.Models;
+
+public class Vault
+{
+    public int Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    [Url] public string Img { get; set; }
+    public bool IsPrivate { get; set; }
+    public string CreatorId { get; set; }
+    public Creator Creator { get; set; }
+}
